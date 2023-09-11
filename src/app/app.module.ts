73f8657aaed3fe7990f63f6c60b0f53f290688fb
Marques Junior <81861759+MarquesJr132@users.noreply.github.com>
2023-09-11 23:03:26 +0200

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
+import {SafePipe, ViewComponent} from './view/view.component';
+import {RouterOutlet} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ChooseComponent } from './choose/choose.component';
+import { DevelopersComponent } from './developers/developers.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+    ViewComponent,
+    ChooseComponent,
+    SafePipe,
+    DevelopersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {path: '', component: MainComponent},
+      {path: 'main', component: MainComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: 'video', component: ViewComponent},
+      {path: 'developers', component: DevelopersComponent}
+    ]),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
